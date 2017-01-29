112b09cf3ef363570370a39ddfa58b477d1830dc
Rafael <rafaelkds@gmail.com>
2017-01-29 00:51:08 -0200

    Post e Get simples no controller

---
+++ src/Api/Controllers/CandidatoController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Api.Context;
+using Api.Entities;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class CandidatoController : Controller
+    {
+        // GET: api/candidato
+        [HttpGet]
+        public IEnumerable<Candidato> Get()
+        {
+            using (var context = (EasyContext)this.HttpContext.RequestServices.GetService(typeof(EasyContext)))
+            {
+                return context.Candidatos.ToArray();            }
+        }
+
+        // GET api/candidato/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/candidato
+        [HttpPost]
+        public void Post([FromBody]Candidato candidato)
+        {
+            using(var context = (EasyContext)this.HttpContext.RequestServices.GetService(typeof(EasyContext)))
+            {
+                context.Candidatos.Add(candidato);
+
+                context.SaveChanges();
+            }
+        }
+
+        // PUT api/candidato/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/candidato/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
